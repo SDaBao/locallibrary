6ff05a53413f6e402da71e6f912989e9faae2a15
Bao <2421443120@qq.com>
2022-07-03 22:54:46 +0800

    update author_page

--- catalog/views.py
+++ catalog/views.py
@@ -25,11 +25,13 @@ from django.views import generic
 
 class BookListView(generic.ListView):
     model = Book
-    context_object_name = 'my_book_list'   # your own name for the list as a template variable
-    queryset = Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
-    template_name = 'books/my_arbitrary_template_name_list.html'  # Specify your own template name/location 
+
 
 class BookDetailView(generic.DetailView):
     model = Book
 
-    
+class AuthorListView(generic.ListView):
+    model = Author
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
